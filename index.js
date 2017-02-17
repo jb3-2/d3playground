8876a2b2622c25f7456cebb617b62abfdf5aed16
Jonas Bieri <jonas.bieri@gmail.com>
2017-02-17 09:22:17 +0000

    Add transistions

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ svg.append('g')            // create a <g> element
   .call(xAxis);            // let the axis do its thing
 
 
-var linebreak = d3.select('body')
+d3.select('body')
     .append('br');
 
 //Second example: data binding (some code changed due to changes in v4)
@@ -74,7 +74,7 @@ newRects.append('rect')
   .attr('height', y.bandwidth())
   .attr('width', d => x(d.count));
 
-
+var oldsales = JSON.parse(JSON.stringify(sales));
 sales.pop();
 //the following line does not work for me
 const rects2 = rects.data(sales);
@@ -82,3 +82,58 @@ const rects2 = rects.data(sales);
 var rectsToRemove = rects2.exit();
 rectsToRemove.remove(); 
 
+
+
+d3.select('body')
+    .append('br');
+    
+//3rd example: transitions
+var days = [];
+days[0] = oldsales;
+var newcounts = [16, 7, 8];
+var newsales = JSON.parse(JSON.stringify(oldsales));
+/*
+newsales[0]['count'] = 16;
+newsales[1]['count'] = 7;
+newsales[2]['count'] = 8;
+*/
+[16, 7, 8].forEach((d,i) => newsales[i]['count'] = d);
+days[1] = newsales;
+sales = days[0];
+
+function toggle() {
+  sales = (sales == days[0]) ? days[1] : days[0];
+  update();
+}
+
+function update() {
+  var rects = svg.selectAll('rect')
+    .data(sales, d => d.product);
+
+  // When we enter, we add the DOM element
+  // and set up the things that won't change
+  var enterRects = rects.enter()
+  /*
+    .append('rect')
+      .attr('x', x(0))
+      .attr('y', d => y(d.product))
+      .attr('height', y.bandwidth())
+  */
+    ;
+
+  // "rects" represents the update selection, we need to
+  // manually merge it with the enter selection to update
+  // all rects at the same time
+  rects.merge(enterRects)
+    .transition()
+    .duration(1000)
+    .attr('width', d => x(d.count))
+    .attr('fill', 'red')
+  ;
+};
+
+var toggleButton = d3.select('body')
+  .append('button')
+  .attr('onClick', 'toggle()')
+  .text('toggle')
+  ;
